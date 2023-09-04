9aee24da16cae005cb48a002fcb454b25df11a87
golddrone <golddrone@naver.com>
2023-09-04 15:57:44 +0900

    git add mapper-config

--- src/main/java/egovframework/appn/model/Ris0210DTO.java
+++ src/main/java/egovframework/appn/model/Ris0210DTO.java
@@ -1,5 +1,40 @@
 package egovframework.appn.model;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
+
+import javax.validation.constraints.Size;
+
+import org.springmodules.validation.bean.conf.loader.annotation.handler.NotBlank;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Ris0210DTO {
-
+	@NotBlank
+	@Size(max = 20)
+	private String hsptId;
+	@NotBlank
+	@Size(max = 20)
+	private String imgnRoomCd;
+	@NotBlank
+	@Size(max = 10)
+	private String wkdy;
+	@NotBlank
+	private LocalTime strtTime;
+	private LocalTime endTime;
+	private Integer appnOutpPssbCnt;
+	private Integer appnInptPssbCnt;
+	private Integer appnHlxmPssbCnt;
+	@Size(max = 20)
+	private String rgstId;
+	private LocalDateTime rgstDttm;
+	@Size(max = 20)
+	private String mdfcId;
+	private LocalDateTime mdfcDttm;
 }
