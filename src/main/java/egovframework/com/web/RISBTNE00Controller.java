74681c886772af9b664745fea6cbbd849a8ddb63
soorin <rin4330@naver.com>
2023-09-07 10:49:15 +0900

    개인페이지 추가 및 twoGrid 버튼 추가

---
+++ src/main/java/egovframework/com/web/RISBTNE00Controller.java
@@ -0,0 +1,17 @@
+package egovframework.com.web;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RISBTNE00Controller {
+	@RequestMapping(value = "/RISBTNE00.do")
+	public String menu(Model model) throws Exception {
+		return ".main/com/RISBTNE00";
+	}
+}
+
