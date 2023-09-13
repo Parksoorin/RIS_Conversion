7e76b0d0b811570af8695158bb55e3460a9770cc
wooyeol <dnduf158@gmail.com>
2023-09-13 15:45:20 +0900

    촬영실별 통계 페이지 필터 조건 추가

--- src/main/java/egovframework/stts/service/SttsService.java
+++ src/main/java/egovframework/stts/service/SttsService.java
@@ -3,8 +3,11 @@ package egovframework.stts.service;
 import java.util.List;
 import java.util.Map;
 
+import egovframework.stts.model.Ris0102DTO;
 import egovframework.stts.model.Ris1201DTO;
 
 public interface SttsService {
-	List<Ris1201DTO> getRis1201List(Map<String, Integer> param);
+	List<Ris1201DTO> getRis1201List(Map<String, String> param);
+	List<Ris0102DTO> getRisImgnList();
+	List<Ris0102DTO> getRisRoomList();
 }
